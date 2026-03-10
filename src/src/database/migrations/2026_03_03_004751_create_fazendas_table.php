7d38ffd5dca6128ba9f142fe3359a879be42c992
Binary-Phantom <profissional.viniciusproenca@gmail.com>
2026-03-10 00:44:41 -0300

    feat: migrations update

--- src/src/database/migrations/2026_03_03_004751_create_fazendas_table.php
+++ src/src/database/migrations/2026_03_03_004751_create_fazendas_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('fazenda', function (Blueprint $table) {
+        Schema::create('fazendas', function (Blueprint $table) {
             $table->id();
-            $table ->unique('nome'); //se der ruim foi aqui que eu mexi
+            //$table ->unique('nome'); //se der ruim foi aqui que eu mexi (Deu ruim)
             $table->timestamps();
         });
     }
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('fazenda');
+        Schema::dropIfExists('fazendas');
     }
 };
--- src/src/database/migrations/2026_03_03_010142_create_veterinarios_table.php
+++ src/src/database/migrations/2026_03_03_010142_create_veterinarios_table.php
@@ -11,8 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('veterinario', function (Blueprint $table) {
+        Schema::create('veterinarios', function (Blueprint $table) {
             $table->id();
+            $table->string('crmv', 5)->primary();
+            $table->string('nome');
             $table->timestamps();
         });
     }
@@ -22,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('veterinario');
+        Schema::dropIfExists('veterinarios');
     }
 };
