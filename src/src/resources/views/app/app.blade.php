733c1b3f948edae6454def9d499a1143985bcc8e
Binary-Phantom <profissional.viniciusproenca@gmail.com>
2026-03-11 11:56:58 -0300

    add: adding mobile viiews compatibility

--- src/src/resources/views/app/app.blade.php
+++ src/src/resources/views/app/app.blade.php
@@ -1,22 +1,34 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <title>Laravel Farm</title>
+<html lang="pt-br">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Laravel Farm</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</head>
+
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 <div class="container">
 
 <a class="navbar-brand" href="{{ route('app') }}">
-Laravel Farm
+🐄 Laravel Farm
 </a>
 
-<ul class="navbar-nav">
+<!-- botão hamburguer (mobile) -->
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
+<span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="menu">
+
+<ul class="navbar-nav ms-auto">
 
 <li class="nav-item">
 <a class="nav-link" href="{{ route('fazendas.index') }}">
@@ -45,6 +57,8 @@ Relatórios
 </ul>
 
 </div>
+
+</div>
 </nav>
 
 <div class="container mt-4">
@@ -54,6 +68,8 @@ Relatórios
 @yield('content')
 
 </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
