a81fadb421cd977e1234d47565cff8f7be9080de
Binary-Phantom <profissional.viniciusproenca@gmail.com>
2026-03-12 01:37:01 -0300

    fix: localhost unable to generate containers due missing dockerfile, compose and .env  templates

--- src/src/start.sh
+++ src/src/start.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
-# start.sh - roda Artisan e Apache em produção
 
-# Garante que as pastas de logs e cache existem
 mkdir -p storage/logs storage/framework/views bootstrap/cache
 chmod -R 777 storage bootstrap/cache
+#resolve alguns conflitos caso n suba na ordem certa, e o vendor seja excluido antes 
+if [ ! -d "vendor" ]; then
+    composer install
+fi
 
-# Garante chave do app
 if ! php artisan key:show > /dev/null 2>&1; then
     php artisan key:generate
 fi
 
-# Limpa e cria caches
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
 
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-
-# Roda migrations antes do app iniciar
 php artisan migrate --force
 
-# Inicia Apache
 apache2-foreground
